0b12aa08fe3c2e6aa2eea5e2321becc1ddac5926
YumaMatsumura <yumapine@gmail.com>
2022-07-17 00:09:53 +0900

    update main.cpp

--- cpp_test/src/main.cpp
+++ cpp_test/src/main.cpp
@@ -1,4 +1,27 @@
-int main()
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/int32.hpp>
+
+int main(int argc, char **srgv)
 {
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("test_node");
+  auto pub = node->create_publisher<std_msgs::msg::Int32>("num", 10);
+
+  rclcpp::WallRate loop(1);
+  std_mss::msg::Int32 msg;
+  msg.data = 0;
+
+  while(rclcpp::ok()){
+    if(msg.data < 10){
+      msg.data++;
+    }else{
+      msg.data = 0;
+    }
+
+    pub->publish(msg);
+    loop.sleep();
+  }
+
+  rclcpp::shutdown();
   return 0;
 }
